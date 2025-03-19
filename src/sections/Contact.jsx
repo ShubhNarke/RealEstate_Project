244719eff1f4166f5b092568647f6f5a7b2c6db0
shubhamnarke <shubhamnarke04@gmail.com>
2025-03-19 17:59:51 +0530

    v1.1

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDarkMode } from '../component/DarkModeContext'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Contact = () => {
+  
+  useEffect(() => {
+        AOS.init({
+          offset: 200,
+          duration: 800,
+          easing: "ease-in-sine",
+          delay: 100,
+        });
+      }, []);
+  
+       const { darkMode } = useDarkMode();
+
   return (
-    <div>Contact</div>
+    <div className={`${darkMode ? 'dark:bg-black': 'light bg-transparent' } pb-20`}>
+      
+    </div>
   )
 }
 
